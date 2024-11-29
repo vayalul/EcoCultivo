148c7e4d7e2f76d6ec87aa83dee9f8a38b1745f3
unknown <va.morales@duocuc.cl>
2024-11-29 17:18:54 -0300

    elimino archivo sin uso, arreglo comentarios

---
+++ EcoCultivoApp/App-ecocultivo/Navigation.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import Miscultivos from "./screens/Miscultivos";
+import Comunidad from "./screens/Comunidad";
+import Mercado from "./screens/Mercado";
+import Profile from "./screens/Profile";
+
+const Tab = createBottomTabNavigator();
+
+function MyTabs() {
+    return (
+        <Tab.Navigator initialRouteName="Miscultivos"
+        screenOptions={{
+            tabBarActiveTintColor: 'green',
+            headerShown: false,
+        }}>
+        <Tab.Screen name="Miscultivos" component={Miscultivos}
+        options={{
+            tabBarLabel: 'Mis Cultivos',
+            tabBarIcon: ({ color, size }) => (
+                <MaterialCommunityIcons name="leaf" size={30} color= {color} />
+            ),
+        }} />
+        <Tab.Screen name="Comunidad" component={Comunidad}
+        options={{
+            tabBarIcon: ({ color, size }) => (
+                <MaterialCommunityIcons name="chat-processing" size={30} color= {color} />
+            ),
+        }} />
+        <Tab.Screen name="Mercado" component={Mercado} 
+        options={{
+            tabBarIcon: ({ color, size }) => (
+                <MaterialCommunityIcons name="cart" size={30} color= {color} />
+            ),
+        }}/>
+        <Tab.Screen name="Perfil" component={Profile}
+        options={{
+            tabBarIcon: ({ color, size }) => (
+                <MaterialCommunityIcons name="account-circle" size={30} color={color} />
+            ),
+        }}/>
+        </Tab.Navigator>
+    );
+}
+
+export default MyTabs;
